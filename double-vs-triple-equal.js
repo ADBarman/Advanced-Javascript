4e9419d2dee343aa0d7bfe0f47fb8bdb6a04fcc2
ADBarman <aumitav2017@gmail.com>
2020-03-07 01:36:49 +0600

    dte

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,80 @@
+const 1st = 2;
+const 2nd = 3;
+if (1st == 2nd){
+	console.log("condition is true");
+}
+else {
+	console.log("condition is false");
+}
+//-----------------------------------------------
+const 1st = 2;
+const 2nd = 2;
+if (1st == 2nd){
+	console.log("condition is true");
+}
+else {
+	console.log("condition is false");
+}
+//--------------------------------------
+const 1st = 2;
+const 2nd = "2";
+if (1st == 2nd){
+	console.log("condition is true");
+}
+else {
+	console.log("condition is false");
+}
+//----------------------------------------------
+const 1st = 2;
+const 2nd = "2";
+if (1st === 2nd){
+	console.log("condition is true");
+}
+else {
+	console.log("condition is false");
+}
+//----------------------------------------
+const 1st = 2;
+const 2nd = true;
+if (1st == 2nd){
+	console.log("condition is true");
+}
+else {
+	console.log("condition is false");
+}
+//----------------------------------------
+const 1st = 1;
+const 2nd = true;
+if (1st == 2nd){
+	console.log("condition is true");
+}
+else {
+	console.log("condition is false");
+}
+//------------------------------------------
+const 1st = 1;
+const 2nd = false
+if (1st == 2nd){
+	console.log("condition is true");
+}
+else {
+	console.log("condition is false");
+}
+//---------------------------------------
+const 1st = 0;
+const 2nd = false;
+if (1st == 2nd){
+	console.log("condition is true");
+}
+else {
+	console.log("condition is false");
+}
+//--------------------------------------------
+const 1st = 0;
+const 2nd = true;
+if (1st == 2nd){
+	console.log("condition is true");
+}
+else {
+	console.log("condition is false");
+}
