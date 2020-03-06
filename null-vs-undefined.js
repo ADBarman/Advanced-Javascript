50d48fe9faa2a472978d533f6d560769e6e11434
ADBarman <aumitav2017@gmail.com>
2020-03-07 00:47:41 +0600

    null

---
+++ null-vs-undefined.js
@@ -0,0 +1,34 @@
+let pakhi;
+console.log(pakhi);
+//------------------------------
+function add(num1, num2){
+	console.log(num1+num2);
+}
+const result = add(13,82);
+console.log(result);
+//------------------------------
+function add(num1, num2){
+	console.log(num1+num2);
+	return
+}
+const result = add(13,82);
+console.log(result);
+//---------------------------------
+function add(num1, num2){
+	console.log(num1, num2);
+}
+const result = add(13);
+console.log(result);
+//-----------------------------------
+const premik = {name: "smart dude", phone:456131};
+console.log(premik.gf);
+//---------------------------------------
+let fun = undefined;
+console.log(fun);
+//----------------------
+//>>its negative or falsy is undefined
+//---------------------------------------------
+//null>>empty or not existance
+
+let ages = [2,5,9];
+console.log(ages[11]);
