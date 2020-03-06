79a2fbe556703f75c7fc3dcd9e1a26155a4265ed
ADBarman <aumitav2017@gmail.com>
2020-03-06 22:19:16 +0600

    true fasle

---
+++ true-false.js
@@ -0,0 +1,120 @@
+const age=4;
+
+if (age>0){
+	console.log("condition is true");
+}
+else{
+	console.log("condition is False");
+}
+//---------------------------------------------------
+const name="Solaiman";
+
+if (name.length>0){
+	console.log("condition is true");
+}
+else{
+	console.log("condition is False");
+}
+//-----------------------------------------------
+const name="Solaiman";
+
+if (name){
+	console.log("condition is true");
+}
+else{
+	console.log("condition is False");
+}
+//-------------------------------------------------
+const name="";
+
+if (name){
+	console.log("condition is true");
+}
+else{
+	console.log("condition is False");
+}
+//------------------------------------------------
+const name=" ";
+
+if (name){
+	console.log("condition is true");
+}
+else{
+	console.log("condition is False");
+}
+//------------------------------------------------
+const name="0";
+
+if (name){
+	console.log("condition is true");
+}
+else{
+	console.log("condition is False");
+}
+//--------------------------------------------
+let name;
+console.log(name);
+if (name){
+	console.log("condition is true");
+}
+else{
+	console.log("condition is False");
+}
+//-------------------------------
+let name=null;
+console.log(name);
+if (name){
+	console.log("condition is true");
+}
+else{
+	console.log("condition is False");
+}
+//----------------------------------------
+let name=NaN;
+console.log(name);
+if (name){
+	console.log("condition is true");
+}
+else{
+	console.log("condition is False");
+}
+//-----------------------------------
+let name={};
+if (name){
+	console.log("condition is true");
+}
+else{
+	console.log("condition is False");
+}
+//-------------------------------------
+let name=false;
+if (name){
+	console.log("condition is true");
+}
+else{
+	console.log("condition is False");
+}
+//------------------------------------
+let name='false';
+if (name){
+	console.log("condition is true");
+}
+else{
+	console.log("condition is False");
+}
+//---------------------------------------
+let name=0;
+if (name || name ==0){
+	console.log("condition is true");
+}
+else{
+	console.log("condition is False");
+}
+//----------------------------------------
+let name=12;
+if (name || name ==0){
+	console.log("condition is true");
+}
+else{
+	console.log("condition is False");
+}
