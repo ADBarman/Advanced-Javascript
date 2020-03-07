845475e2a15097460c725276af41bc2730e52794
ADBarman <aumitav2017@gmail.com>
2020-03-07 13:20:05 +0600

    op

---
+++ object-property.js
@@ -0,0 +1,45 @@
+const students = [
+	{id:21, name:'Omar Sunny'}
+	{id:31, name:'manaaaaa'}
+	{id:41, 'moina'}
+	{id:71, name:'deejol'}
+];
+
+const names = students.map(s=> s.name);
+console.log(names);
+//-----------------------------------
+const students = [
+	{id:21, name:'Omar Sunny'}
+	{id:31, name:'manaaaaa'}
+	{id:41, 'moina'}
+	{id:71, name:'deejol'}
+];
+
+const names = students.map(s=> s.name);
+const names = students.map(s=> s.id);
+console.log(ids);
+//-----------------------------------------
+const students = [
+	{id:21, name:'Omar Sunny'}
+	{id:31, name:'manaaaaa'}
+	{id:41, 'moina'}
+	{id:71, name:'deejol'}
+];
+
+const names = students.map(s=> s.name);
+const names = students.map(s=> s.id);
+const bigger = students.filter(s => s.id>40);
+console.log(bigger);
+//---------------------------------------
+const students = [
+	{id:21, name:'Omar Sunny'}
+	{id:31, name:'manaaaaa'}
+	{id:41, 'moina'}
+	{id:71, name:'deejol'}
+];
+
+const names = students.map(s=> s.name);
+const names = students.map(s=> s.id);
+const bigger = students.filter(s => s.id>40);
+const biggerOne = students.find(s => s.id>40);
+console.log(biggerOne);
